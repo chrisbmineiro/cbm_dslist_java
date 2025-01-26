4151e3d9a7e023e4c259196e3553fc9210d7c3a0
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-25 21:36:00 -0300

    feat: add GameMinDTO, GameService, GameRepository and GameController

---
+++ src/main/java/com/cbm/dslist/service/GameService.java
@@ -0,0 +1,22 @@
+package com.cbm.dslist.service;
+
+import com.cbm.dslist.dto.GameMinDTO;
+import com.cbm.dslist.entities.Game;
+import com.cbm.dslist.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
+        return dto;
+    }
+}
